65abc06c9483322653391c4e07a048e8fc07783f
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-09-09 20:01:48 -0500

    Threw in scaffold for constrained sparsify. Notes added where bugs expected

--- dimspan/sindy/constrainedSparsify.py
+++ dimspan/sindy/constrainedSparsify.py
@@ -2,13 +2,73 @@ from functools import reduce
 from math import factorial
 import numpy as np
 
+def constrainedSparsify(constraints, polyorder, modes, theta, dX, _lambda, dimensions):
+  """
+  Sparsify Xi, but with special constraints for known variables.abs
+
+  Constraints : array of tuples representing the functions to fix
+    - ex: [['a','b'],['ab']] -> fix the functions dx/dt = x + y, and dy/dt = xy
+      for some parameters 
+  """
+
+  matches = _constraintMatches(len(constraints), polyorder)
+  print("matches", matches)
+
+  constrainedXi = _marshalXi(constraints, polyorder, modes)
+
+  xi, residual, _, _ = np.linalg.lstsq(theta, dX)
+  lowestResidual = np.sum(lowestResidual)
+  bestXi = list(xi)
+
+  for toFix in matches:
+    xi, _, _, _ = np.linalg.lstsq(theta, dX)
+
+    # TODO : make sure this isn't jenky
+    xi[:, toFix] = constrainedXi
+    
+    for i in range(10):
+      xi = _regularize(xi, _lambda)
+      xi, residual = _iterateRegression(xi, theta, dX, _lambda, dimensions)
+    
+      currentResidual = np.sum(residual)
+
+      if currentResidual < lowestResidual:
+        lowestResidual = currentResidual
+        bestXi = list(xi)
+
+    return bestXi, lowestResidual
+
+def _regularize(xi, _lambda):
+  "clips small elements (to zero)"
+  smallIdx = abs(xi)<=_lambda
+  xi[smallIdx] = 0
+  return xi
+
+def _iterateRegression(xi, theta, dX, _lambda, dimensions):
+  "performs least-squares iteration on a subset of indices above lambda threshold"
+  if dimensions == 1:
+    bigIdx = abs(xi[:]) > _lambda
+    tempXi, _, _, _ = np.linalg.lstsq(theta[:, bigIdx], dX)
+    xi[bigIdx] = tempXi
+    
+  else:
+    for dim in range(dimensions):
+      # bigIdx = [i for i,v in enumerate(xi[:, dim]) if abs(v) > _lambda]
+      bigIdx = abs(xi[:, dim]) > _lambda
+      tempXi, residuals, _, _ = np.linalg.lstsq(theta[:, bigIdx], dX[:, dim])
+      xi[bigIdx, dim] = tempXi
+  
+  return xi, residuals
+
 def _sumOfComboWithReplacement(total, options):
+  "Helper function that gives reference for polynomial functions counts"
   count = 1
   for i in range(1, total+1):
     count += factorial(options+i-1) / (factorial(i) * factorial(options-1))
   return int(count)
   
 def _stringSum(string):
+  "Helper function to condense function string to numerical representation"
   chars = list(string)
   ints = list(map(lambda x: (int(format(ord(x), "x"))-61), chars))
   return reduce(
@@ -16,11 +76,13 @@ def _stringSum(string):
   )
 
 def _indexForString(string, modes):
+  "Returns the Xi index for a given string"
   order = len(string)
   ref = _sumOfComboWithReplacement(order-1, modes)
   return ref + _stringSum(string)
 
 def _marshalXi(constraints, order, modes):
+  "Converts string representation to sparse representation"
   rowCount = len(constraints)
   colCount = _sumOfComboWithReplacement(order, modes)
   xi = np.zeros((rowCount,colCount))
@@ -32,55 +94,14 @@ def _marshalXi(constraints, order, modes):
   
   return xi.T
 
-def constrainedSparsify(constraints, polyorder, modes, theta, dX, _lambda, dimensions):
-
-  matches = _constraintMatches(len(constraints), 4)
-  print("matches", matches)
-
-  constrainedXi = _marshalXi(constraints, polyorder, modes)
-  xi, _, _, _ = np.linalg.lstsq(theta, dX)
-  
-  for i in range(10):
-    xi = _regularize(xi, _lambda)
-    xi = _iterateRegression(xi, theta, dX, _lambda, dimensions)
-  return xi
-
-
-def _regularize(xi, _lambda):
-  "clips small elements (to zero)"
-
-  smallIdx = abs(xi)<=_lambda
-  xi[smallIdx] = 0
-  return xi
-
-def _iterateRegression(xi, theta, dX, _lambda, dimensions):
-  "performs least-squares iteration on a subset of indices above lambda threshold"
-
-  if dimensions == 1:
-    bigIdx = abs(xi[:]) > _lambda
-    tempXi, _, _, _ = np.linalg.lstsq(theta[:, bigIdx], dX)
-    xi[bigIdx] = tempXi
-    
-  else:
-    for dim in range(dimensions):
-      # bigIdx = [i for i,v in enumerate(xi[:, dim]) if abs(v) > _lambda]
-      bigIdx = abs(xi[:, dim]) > _lambda
-      tempXi, _, _, _ = np.linalg.lstsq(theta[:, bigIdx], dX[:, dim])
-      xi[bigIdx, dim] = tempXi
-  
-  return xi
-
 def _constraintMatches(fixed, order):
-
-  if fixed > order:
-    return
-
-  remaining = list(range(order+1))
-  current = []
-  matches = []
-
+  """
+  Given the number of functions we want to sparsily fix, and the total
+  possible number of dimensions we are optimizing over, return an array holding
+  tuples of all of the possible sets of indices for which we can fix
+  for that optimization.
+  """
   def innerRecurse(current, remaining):
-    
     if current:
       if len(current) == fixed:
         matches.append(current)
@@ -97,8 +118,6 @@ def _constraintMatches(fixed, order):
       print("recurse", newCopy, newRemaining)
       innerRecurse(newCopy, newRemaining)
 
-  innerRecurse([], remaining)
-
   def duplicate(arr):
     copy = list(arr)
     copy.sort()
@@ -106,8 +125,14 @@ def _constraintMatches(fixed, order):
       if copy[idx] == copy[idx - 1]:
         return False
     return True
+
+  if fixed > order:
+    return
+
+  remaining = list(range(order+1))
+  current = []
+  matches = []
+
+  innerRecurse([], remaining)
     
   return list(filter((lambda arr: duplicate(arr)), matches))
-
-
-
